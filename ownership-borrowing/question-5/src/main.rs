cb9feb9f96c74bf6f901f17c57aafdde3a7a508a
Justin Cabrera <justincrypto702@gmail.com>
2025-12-29 15:22:20 -0800

    Enhance main.rs with borrowing rules comments
    Added comments explaining the rules of borrowing in Rust.

--- ownership-borrowing/question-5/src/main.rs
+++ ownership-borrowing/question-5/src/main.rs
@@ -5,3 +5,12 @@ fn main() {
     let r2 = &mut s; //Mutable borrow
     println!("{}, {}", r1, r2);
 }
+
+// immutable + mutable
+// -prevents data from being changed while someone else is reading it
+// -you cannot have a mutable reference while any immutable references
+//  are still active or in use
+// -this rule exists to ensure data integrity
+// -the data could suddenly change without warning
+// -by blocking this, Rust guarantees that as long as you have a read-
+//  only view of data, that data will remain constant
